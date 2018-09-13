437cf75af60c5d701f571e788e024a1b8ec44baa
CSA-JDM <33459798+CSA-JDM@users.noreply.github.com>
2018-09-13 11:49:21 -0500

    9/13/2018
    in-class

--- favorite_ice_cream.py
+++ favorite_ice_cream.py
@@ -20,26 +20,31 @@ class App(tk.Frame):
         self.labels["title_label"] = tk.Label(self, text="Favorite Ice Cream")
         self.labels["title_label"].pack()
 
-        self.buttons["clear_button"] = tk.Button(self, text="Clear", state="disabled")
-        self.buttons["clear_button"].pack()
-        self.buttons["submit_button"] = tk.Button(self, text="Submit", state="disabled")
-        self.buttons["submit_button"].pack()
-
         self.int_vars["chocolate_state"] = tk.IntVar()
-        self.check_buttons["chocolate_check_button"] = tk.Checkbutton(self, text="Chocolate", variable=self.int_vars["chocolate_state"])
+        self.check_buttons["chocolate_check_button"] = tk.Checkbutton(self, text="Chocolate",
+                                                                      variable=self.int_vars["chocolate_state"])
         self.check_buttons["chocolate_check_button"].pack()
         self.int_vars["vanilla_state"] = tk.IntVar()
-        self.check_buttons["vanilla_check_button"] = tk.Checkbutton(self, text="Vanilla", variable=self.int_vars["vanilla_state"])
+        self.check_buttons["vanilla_check_button"] = tk.Checkbutton(self, text="Vanilla",
+                                                                    variable=self.int_vars["vanilla_state"])
         self.check_buttons["vanilla_check_button"].pack()
         self.int_vars["mint_state"] = tk.IntVar()
-        self.check_buttons["mint_check_button"] = tk.Checkbutton(self, text="Mint", variable=self.int_vars["mint_state"])
+        self.check_buttons["mint_check_button"] = tk.Checkbutton(self, text="Mint",
+                                                                 variable=self.int_vars["mint_state"])
         self.check_buttons["mint_check_button"].pack()
         self.int_vars["cookie_state"] = tk.IntVar()
-        self.check_buttons["cookie_check_button"] = tk.Checkbutton(self, text="Cookie", variable=self.int_vars["cookie_state"])
+        self.check_buttons["cookie_check_button"] = tk.Checkbutton(self, text="Cookie",
+                                                                   variable=self.int_vars["cookie_state"])
         self.check_buttons["cookie_check_button"].pack()
         self.int_vars["strawberry_state"] = tk.IntVar()
-        self.check_buttons["strawberry_check_button"] = tk.Checkbutton(self, text="Strawberry", variable=self.int_vars["strawberry_state"])
+        self.check_buttons["strawberry_check_button"] = tk.Checkbutton(self, text="Strawberry",
+                                                                       variable=self.int_vars["strawberry_state"])
         self.check_buttons["strawberry_check_button"].pack()
+
+        self.buttons["submit_button"] = tk.Button(self, text="Submit", state="disabled", command=self.submit_choices)
+        self.buttons["submit_button"].pack(side="right")
+        self.buttons["clear_button"] = tk.Button(self, text="Clear", state="disabled", command=self.clear_choices)
+        self.buttons["clear_button"].pack(side="right")
 
         self.clear_and_submit_check()
 
@@ -49,7 +54,29 @@ class App(tk.Frame):
                     "disabled" in self.buttons["clear_button"].config("state"):
                 self.buttons["submit_button"].config(state="normal")
                 self.buttons["clear_button"].config(state="normal")
+        else:
+            self.buttons["submit_button"].config(state="disabled")
+            self.buttons["clear_button"].config(state="disabled")
         self.after(1, self.clear_and_submit_check)
+
+    def submit_choices(self):
+        values = [[x, self.int_vars[f'{x.split("_")[0]}_state'].get()] for x in self.check_buttons.keys()]
+        favorite_ice_flavors = favorite_ice_info.split("\n")
+        favorite_ice_flavors = [x.split(": ") for x in favorite_ice_flavors]
+        for name, value in values:
+            if value == 1:
+                favorite_ice_flavors[
+                    [x for x in range(len(favorite_ice_flavors)) if favorite_ice_flavors[x][0] == name.split("_")[0]][
+                        0]][1] = int(favorite_ice_flavors[[x for x in range(len(favorite_ice_flavors)) if favorite_ice_flavors[x][0] == name.split("_")[0]][0]][1])
+                favorite_ice_flavors[[x for x in range(len(favorite_ice_flavors)) if favorite_ice_flavors[x][0] == name.split("_")[0]][0]][1] += 1
+                # favorite_ice_txt = open("favorite_ice.txt", "w")
+                print("".join(value_) for value_ in values)
+                # favorite_ice_txt.write("".join(values))
+                # favorite_ice_txt.close()
+
+    def clear_choices(self):
+        for x in self.check_buttons.keys():
+            self.int_vars[f'{x.split("_")[0]}_state'].set(0)
 
 
 if __name__ == "__main__":
