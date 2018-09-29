70af24a7c37f83d44b18d71462af4ec6812d26fb
Jacob Meadows <2178@chaparralstaracademy.com>
2018-09-28 19:02:55 -0500

    9/27/2018
    at home (delayed push)

--- simple_grid.py
+++ simple_grid.py
@@ -8,12 +8,13 @@ class App(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         self.master = master
-        self.master.config(width=240, height=185)
+        self.master.config(width=200, height=60)
         self.master.title("Simple Grid")
-        self.config(width=240, height=185)
+        self.config(width=200, height=60)
         self.place(x=0, y=0)
         self.grid_propagate(0)
 
+        self.frames = dict()
         self.labels = dict()
         self.buttons = dict()
         self.check_buttons = dict()
@@ -26,14 +27,19 @@ class App(tk.Frame):
         self.widgets = [self.labels, self.buttons, self.check_buttons, self.combo_boxes, self.entries,
                         self.radio_buttons, self.str_vars, self.int_vars, self.vars]
 
-        self.labels["name_label"] = tk.Label(self, text="Name")
-        self.labels["name_label"].grid(column=0, row=0, sticky="ne")
-        self.entries["right_entry"] = tk.Entry(self)
-        self.entries["right_entry"].grid(column=1, row=0, sticky="nw")
-        self.buttons["cancel_button"] = tk.Button(self, text="Cancel")
-        self.buttons["cancel_button"].grid(column=0, row=1, sticky="n")
-        self.buttons["clear_button"] = tk.Button(self, text="Clear")
-        self.buttons["clear_button"].grid(column=1, row=1, sticky="n")
+        self.frames["main_frame"] = tk.Frame(self)
+        self.frames["main_frame"].grid(column=0, row=0)
+        self.labels["name_label"] = tk.Label(self.frames["main_frame"], text="Name")
+        self.labels["name_label"].grid(column=0, row=0)
+        self.str_vars["name_str"] = tk.StringVar()
+        self.entries["name_entry"] = tk.Entry(self.frames["main_frame"], textvariable=self.str_vars["name_str"])
+        self.entries["name_entry"].grid(column=1, row=0, columnspan=3)
+        self.buttons["cancel_button"] = tk.Button(self.frames["main_frame"], text="Cancel", command=self.clear_name)
+        self.buttons["cancel_button"].grid(column=0, row=1, pady=5)
+        self.buttons["clear_button"] = tk.Button(self.frames["main_frame"], text="Clear", command=self.clear_name)
+        self.buttons["clear_button"].grid(column=1, row=1, pady=5)
+        self.buttons["submit_button"] = tk.Button(self.frames["main_frame"], text="Submit", command=self.clear_name)
+        self.buttons["submit_button"].grid(column=2, row=1, pady=5)
 
         for column in range(self.grid_size()[0]):
             self.grid_columnconfigure(column, weight=1)
@@ -42,9 +48,14 @@ class App(tk.Frame):
 
         self.update_frame()
 
+    def clear_name(self):
+        self.str_vars["name_str"].set("")
+
     def update_frame(self):
         if self.config("width")[-1] != self.master.geometry().split("x")[0]:
             self.config(width=self.master.geometry().split("x")[0])
+        if self.config("height")[-1] != self.master.geometry().split("y")[0]:
+            self.config(height=self.master.geometry().split("x")[1].split("+")[0])
         self.after(1, self.update_frame)
 
 
