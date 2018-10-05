943bd6f1cff7af0e6f6a5372c8e1edcae4ef63b7
CSA-JDM <33459798+CSA-JDM@users.noreply.github.com>
2018-10-05 11:48:36 -0500

    10/5/2018
    in-class

--- pricing_calculator.py
+++ pricing_calculator.py
@@ -2,12 +2,14 @@
 # Computer Programming II, 6th Period
 # 01 October 2018
 import tkinter as tk
+from tkinter import ttk
 
 
 class App(tk.Frame):
     def __init__(self, master):
         self.master = master
         self.master.config(width=1280, height=720)
+        self.master.title("Pricing")
         super().__init__(self.master, width=1280, height=720)
         self.place(x=0, y=0)
 
@@ -17,19 +19,23 @@ class App(tk.Frame):
         self.labels = dict()
         self.buttons = dict()
         self.str_vars = dict()
+        self.vars = dict()
         self.widgets = [self.list_boxes, self.combo_boxes, self.progress_bars, self.labels, self.buttons, self.str_vars]
 
+        self.vars["item_dict"] = {}
         self.widget_init()
+
         self.submit_check()
 
     def widget_init(self):
+        self.combo_boxes["shipping_combo_box"] = tk.ttk.Combobox(self, values=["UPS", "FedEx", "Express"])
+        self.combo_boxes["shipping_combo_box"].place(x=5, y=5)
         self.list_boxes["top_list_box"] = tk.Listbox(self)
-        self.list_box_update(self.list_boxes["top_list_box"], "Python 3.6\nPython 3.7")
-        self.list_boxes["top_list_box"].place(x=0, y=0)
-        self.buttons["submit_button"] = tk.Button(self, text="Submit")
+        self.list_boxes["top_list_box"].place(x=5, y=30)
+        self.buttons["submit_button"] = tk.Button(self, text="Submit", command=self.submit_check)
         self.buttons["submit_button"].place(x=5, y=500)
-        self.buttons["clear_button"] = tk.Button(self, text="Clear")
-        self.buttons["clear_button"].place(x=100, y=500)
+        self.buttons["clear_button"] = tk.Button(self, text="Clear", command=self.clear_command)
+        self.buttons["clear_button"].place(x=55, y=500)
 
     def submit_check(self):
         if None is not None:
@@ -40,22 +46,12 @@ class App(tk.Frame):
         self.after(1, self.submit_check)
 
     def clear_command(self):
-        self.master.config(height=185)
         for widget in self.winfo_children():
             widget.place_forget()
         self.children.clear()
         for widget_dict in self.widgets:
             widget_dict.clear()
         self.widget_init()
-
-    @staticmethod
-    def list_box_update(list_box, text):
-        if isinstance(text, str):
-            for string in text.split("\n"):
-                list_box.insert(len(list_box.get(0, tk.END)), string)
-        elif isinstance(text, list) or isinstance(text, tuple):
-            for string in text:
-                list_box.insert(len(list_box.get(0, tk.END)), string)
 
     def __str__(self):
         pass
