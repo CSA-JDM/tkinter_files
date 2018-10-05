a0cb7d36eadc21c2be61ede94ab7f81be6647777
Jacob Meadows <2178@chaparralstaracademy.com>
2018-10-04 22:30:56 -0500

    10/4/2018
    at home

--- pricing_calculator.py
+++ pricing_calculator.py
@@ -12,15 +12,24 @@ class App(tk.Frame):
         self.place(x=0, y=0)
 
         self.list_boxes = dict()
+        self.combo_boxes = dict()
+        self.progress_bars = dict()
+        self.labels = dict()
         self.buttons = dict()
         self.str_vars = dict()
+        self.widgets = [self.list_boxes, self.combo_boxes, self.progress_bars, self.labels, self.buttons, self.str_vars]
 
+        self.widget_init()
+        self.submit_check()
+
+    def widget_init(self):
         self.list_boxes["top_list_box"] = tk.Listbox(self)
         self.list_box_update(self.list_boxes["top_list_box"], "Python 3.6\nPython 3.7")
         self.list_boxes["top_list_box"].place(x=0, y=0)
         self.buttons["submit_button"] = tk.Button(self, text="Submit")
-
-        self.submit_check()
+        self.buttons["submit_button"].place(x=5, y=500)
+        self.buttons["clear_button"] = tk.Button(self, text="Clear")
+        self.buttons["clear_button"].place(x=100, y=500)
 
     def submit_check(self):
         if None is not None:
@@ -30,7 +39,17 @@ class App(tk.Frame):
                 self.buttons["submit_button"].config(state="disabled")
         self.after(1, self.submit_check)
 
-    def list_box_update(self, list_box, text):
+    def clear_command(self):
+        self.master.config(height=185)
+        for widget in self.winfo_children():
+            widget.place_forget()
+        self.children.clear()
+        for widget_dict in self.widgets:
+            widget_dict.clear()
+        self.widget_init()
+
+    @staticmethod
+    def list_box_update(list_box, text):
         if isinstance(text, str):
             for string in text.split("\n"):
                 list_box.insert(len(list_box.get(0, tk.END)), string)
